1eda7493a66a2a5fb20a9d0829e8b4bb2937c266
Akash <akashikur011@gmail.com>
2024-08-25 12:18:02 +0530

    first commit

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -2,54 +2,73 @@ import express from "express";
 import db from "@repo/db/client";
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.post("/hdfcWebhook", async (req, res) => {
-    //TODO: Add zod validation here?
-    //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
-    const paymentInformation: {
-        token: string;
-        userId: string;
-        amount: string
-    } = {
-        token: req.body.token,
-        userId: req.body.user_identifier,
-        amount: req.body.amount
-    };
+  const paymentInformation = {
+    token: req.body.token,
+    userId: req.body.user_identifier,
+    amount: req.body.amount,
+  };
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        id: Number(paymentInformation.userId),
+      },
+    });
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+    const userBalance = await db.balance.findUnique({
+      where: {
+        userId: Number(paymentInformation.userId),
+      },
+    });
 
-    try {
-        await db.$transaction([
-            db.balance.updateMany({
-                where: {
-                    userId: Number(paymentInformation.userId)
-                },
-                data: {
-                    amount: {
-                        // You can also get this from your DB
-                        increment: Number(paymentInformation.amount)
-                    }
-                }
-            }),
-            db.onRampTransaction.updateMany({
-                where: {
-                    token: paymentInformation.token
-                }, 
-                data: {
-                    status: "Success",
-                }
-            })
-        ]);
-
-        res.json({
-            message: "Captured"
-        })
-    } catch(e) {
-        console.error(e);
-        res.status(411).json({
-            message: "Error while processing webhook"
-        })
+    if (!userBalance) {
+      await db.balance.create({
+        data: {
+          userId: Number(paymentInformation.userId),
+          amount: Number(paymentInformation.amount),
+          locked: 0,
+        },
+      });
+    } else {
+      await db.$transaction([
+        db.balance.updateMany({
+          where: {
+            userId: Number(paymentInformation.userId),
+          },
+          data: {
+            amount: {
+              increment: Number(paymentInformation.amount),
+            },
+          },
+        }),
+        db.onRampTransaction.updateMany({
+          where: {
+            token: paymentInformation.token,
+          },
+          data: {
+            status: "Success",
+          },
+        }),
+      ]);
     }
 
-})
+    res.json({
+      message: "Captured",
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({
+      message: "Error while processing webhook",
+    });
+  }
+});
 
-app.listen(3003);
+app.listen(3003, () => {
+  console.log("Server is running on port 3003");
+});
---
+++ apps/user-app/app/api/signup/route.ts
@@ -0,0 +1,43 @@
+import { NextApiRequest } from "next";
+import db from "@repo/db/client";
+import bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
+
+export const POST = async (req: NextApiRequest) => {
+  const body = await req.json();
+  const { name, phone, password, email } = body;
+  console.log(name);
+  try {
+    const existingUser = await db.user.findUnique({
+      where: {
+        number: phone,
+      },
+    });
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "User already exists" },
+        { status: 400 }
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await db.user.create({
+      data: {
+        name,
+        number: phone,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return NextResponse.json(
+      { message: "User created", user: newUser },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+};
